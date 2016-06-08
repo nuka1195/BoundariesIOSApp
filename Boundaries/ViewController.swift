5a0c338841eb979d25ecb1cec09c4f5b799d6700
nuka1195 <nuka1195@gmail.com>
2016-06-08 12:57:38 -0600

    Initial Commit

---
+++ Boundaries/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boundaries
+//
+//  Created by Raymond Scott Johns on 6/8/16.
+//  Copyright © 2016 Raymond Scott Johns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
